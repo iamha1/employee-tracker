257feaf80b8fc9e6d9b78b5886ad6f5ce245b9e5
Ha Nguyen <nguyenthingocha2@gmail.com>
2020-03-04 23:38:11 -0500

    add README, run all questions again, create function to generate the output folder and the team html

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const path = require("path");
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
+const outputPath = path.resolve(__dirname, "output", "team.html");
+const render = require('./lib/htmlRenderer')
+
 const allEmployee = [];
-
-
-
 const inquirer = require("inquirer");
 const fs = require("fs");
-​const render = require('./lib/htmlRenderer');
-
 
 const employeeQuestions = [
     {
@@ -62,8 +60,7 @@ const engineerQuestions = [
     }
 ];
 
-
-  const newTeamMemberQuestions = [
+const newTeamMemberQuestions = [
        {
         type: "list",
         message: "Would you like to add more team member?",
@@ -110,7 +107,7 @@ await inquirer
                 template = render(allEmployee);
                 fs.writeFile(outputPath, template, err => {
                     if(err) {
-                        throw err
+                        throw errz
                     }
                     }
                 )
